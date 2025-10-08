3a9cb1a41ec7d9ce214195661d6e098a8b5d4a4c
sumiya-banu <sumiyadudekula2002@gmail.com>
2025-10-08 18:38:16 +0530

    Update greetings.c

--- greetings.c
+++ greetings.c
@@ -4,6 +4,6 @@ int main()
 <<<<<<< HEAD
     printf("hello good morning from jahnavi branch");
 =======
-    printf("hello good morning");
+    printf("hello good morning from sumiya");
 >>>>>>> 0a20da0 (Edited greetings.c in Jahnavi branch)
 }
